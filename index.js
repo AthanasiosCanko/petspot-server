cc8f407cffa8b601f0f38644e9ac42a9531f3bb9
Athanasios <cankoathanasios@gmail.com>
2018-05-08 17:20:15 +0200

    Adding bcrypt to hash passwords...

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var bodyparser = require('body-parser')
 var mongoose = require('mongoose')
+var bcrypt = require('bcrypt')
 
 var db_name = process.env.DB_NAME || require('./data').db_name
 var db_password = process.env.DB_PASSWORD || require('./data').db_password
@@ -30,31 +31,88 @@ app.get('/', function(req, res) {
     })
 })
 
+app.get('/:username/:password', function(req, res) {
+    const username = req.params.username
+    const password = req.params.password
+
+    // All responses - ERROR, INCORRECT_USERNAME, INCORRECT_PASSWORD, CORRECT_CREDENTIALS
+
+    // Search by username
+    user.findOne({username: username}, function(err, item) {
+        if (err) {
+            console.log(err)
+            res.json({message: 'ERROR'})
+        }
+        else if (!item) res.json({message: 'INCORRECT_USERNAME'})
+        else {
+            let hash = item.password
+
+            // If username found, compare password and hash
+            bcrypt.compare(password, hash, function(err, result) {
+                if (err) {
+                    console.log(err)
+                    res.json({message: 'ERROR'})
+                }
+                else {
+                    if (result) res.json({message: 'CORRECT_CREDENTIALS'})
+                    else res.json({message: 'INCORRECT_PASSWORD'})
+                }
+            })
+        }
+    })
+})
+
 app.post('/sign_up', function(req, res) {
     const username = req.body.username
     const email = req.body.email
     const password = req.body.password
     const date = Date.now()
 
-    // All responses - ERROR, NOT_CREATED, CREATED, FOUND
+    // All responses - ERROR, NO_HASH, NOT_CREATED, CREATED, FOUND
+
+    // Search by username first
     user.findOne({username: username}, function(err, item) {
         if (err) {
             console.log(err)
             res.json({message: 'ERROR'})
         }
         else if (!item) {
-            user.create({
-                username: username,
-                email: email,
-                password: password,
-                date: date
-            }, function(err, item) {
+
+            // Search by email next
+            user.findOne({email: email}, function(err, item) {
                 if (err) {
                     console.log(err)
                     res.json({message: 'ERROR'})
                 }
-                else if (!item) res.json({message: 'NOT_CREATED'})
-                else res.json({message: 'CREATED'})
+                else if (!item) {
+
+                    // Hash password if no user exists
+                    bcrypt.hash(password, 12, function(err, hash) {
+                        if (err) {
+                            console.log(err)
+                            res.json({message: 'NO_HASH'})
+                        }
+                        else {
+                            console.log(hash)
+
+                            // If hash successful, create user
+                            user.create({
+                                username: username,
+                                email: email,
+                                password: hash,
+                                date: date
+                            }, function(err, item) {
+                                if (err) {
+                                    console.log(err)
+                                    res.json({message: 'ERROR'})
+                                }
+                                else if (!item) res.json({message: 'NOT_CREATED'})
+                                else res.json({message: 'CREATED'})
+                            })
+                        }
+                    })
+                }
+                else res.json({message: 'FOUND'})
             })
         }
         else res.json({message: 'FOUND'})
